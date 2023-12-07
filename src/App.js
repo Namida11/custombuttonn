a2518bf1a43926ce31e70762cbb3d8b30e3b02a3
Namida11 <109894273+Namida11@users.noreply.github.com>
2023-12-07 10:03:35 +0400

    added all files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CustomButton from "./CustomButton";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <CustomButton />
+    </div>
+  );
+};
+
+export default App;
